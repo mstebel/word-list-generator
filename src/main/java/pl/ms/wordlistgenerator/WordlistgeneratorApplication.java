68acbe9f7a32d24b1bd37db6e738ef47d99ddc30
Magda <m.stebel@mailplus.pl>
2022-06-18 17:38:00 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/ms/wordlistgenerator/WordlistgeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.ms.wordlistgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlistgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordlistgeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ms/wordlistgenerator/WordlistgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ms.wordlistgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordlistgeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
